fcad6582ff56802c399aa4254a859ef31f08e403
eyonys <eyonys@gmail.com>
2023-09-09 13:45:58 -0400

    Service Status

---
+++ NowPayments.Net.Objects/ServiceStatus.cs
@@ -0,0 +1,10 @@
+﻿using System.Text.Json.Serialization;
+
+namespace NowPayments.Net.Objects
+{
+	public class ServiceStatus
+	{
+		[JsonPropertyName("message")]
+		public string Message { get; set; } = "OK";
+	}
+}
